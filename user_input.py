79293a2004cd87ab7ad4fd89dacc54b3ddbe87b2
unknown <lillianngoiri67@gmail.com>
2024-02-25 12:41:48 +0300

    My first python simple code to capture user input

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Enter your first name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+print("Hello", name + ",", "you are", age, "years old and you come from", location + ".")
